e78f28120334be5607469b52cdf23e7adcb99154
Max Brown <mb39@sanger.ac.uk>
2022-04-06 15:58:16 +0100

    Test commit again.

--- publish_site.bash
+++ publish_site.bash
@@ -15,12 +15,14 @@ git branch -D gh-pages
 # and locally I guess...
 git subtree push --prefix public origin gh-pages
 
+# sleep because I think the previous command is being overwritten.
+echo "Deleted old branch. Make new branch."
+sleep 5
+
 # generate your token here:
 # https://github.com/settings/tokens
 TOKEN=$(cat TOKEN)
 
-# sleep because I think the previous command is being overwritten.
-sleep 5
 # and switch the source branch to github pages.
 curl \
   -i \
@@ -30,6 +32,10 @@ curl \
   https://api.github.com/repos/Euphrasiologist/euphrasiologist.github.io/pages \
   -d '{"source":{"branch":"gh-pages"}}'
 
+# sleep because I think the previous command is being overwritten.
+echo "Added source branch == gh-pages."
+sleep 5
+
 # now add my CNAME back to the repo
 curl \
   -i \
@@ -38,3 +44,5 @@ curl \
   -H "Accept: application/vnd.github.v3+json" \
   https://api.github.com/repos/Euphrasiologist/euphrasiologist.github.io/pages \
   -d '{"cname": "maxbrown.xyz"}'
+
+echo "Added cname == maxbrown.xyz"
